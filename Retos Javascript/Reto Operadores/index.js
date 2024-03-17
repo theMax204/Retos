29501d363b3d6abb127468a653c107fd27b4e622
theMax204 <123708158+theMax204@users.noreply.github.com>
2024-03-17 13:16:48 -0600

    Reto operadores js

---
+++ Retos Javascript/Reto Operadores/index.js
@@ -0,0 +1,25 @@
+let numMa = 1 < 2;
+let numMe = 1 > 2;
+let numIgu = 1 == 2;
+let tex1 = "texto 1" == "texto 2";
+let objeto = {nombre: "Maximiliano",edad: 2}
+let objeto2 = {nombre: "Julian",edad: 27}
+let and = 2 === 2 && 3 === 4;
+let or = 2 === 4 || 2 === 2;
+let suma = 10 + 3;
+let resta = 10 - 3;
+let multipli = 10 * 30;
+let divi = 10 / 3;
+let modulo = 10 % 3;
+console.log("El numero 1 es mayor que el numero 2? " + numMa)
+console.log("El numero 1 es menor que el numero 2? " + numMe)
+console.log("El numero 1 es igual al numero 2? " + numIgu)
+console.log("El texto 1 es igual al texto 2? " + tex1)
+console.log("Los objetos son iguales? " + (objeto == objeto2))
+console.log("AND logico: " + and)
+console.log("OR logico: " + or)
+console.log("La suma de 10 + 3: " + suma)
+console.log("La resta de 10 - 3: " + resta)
+console.log("La multiplicacion de 10 * 3: " + multipli)
+console.log("La division de 10 / 3: " + divi)
+console.log("10 modulo de 3: " + modulo)
